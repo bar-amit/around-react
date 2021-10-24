d5d4c199199384c6a77b54d4d58352ec20e06a0b
bar-amit <bar.amit@outlook.com>
2021-10-24 16:12:51 +0300

    feat(Main): fetch user info from api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+import api from '../utils/Api';
 import avatar from '../images/profile__image.jpg';
 
 /**
@@ -12,16 +14,31 @@ import avatar from '../images/profile__image.jpg';
 
 function Main(props) {
   const { onEditProfileClick, onAddPlaceClick, onEditAvatarClick } = props;
+
+  const [ userName, setUserName ] = React.useState(undefined);
+  const [ userDecription, setUserDescription ] = React.useState(undefined);
+  const [ userAvatar, setUserAvatar ] = React.useState(undefined);
+
+  useEffect(() => {
+    api.getUserInfo()
+    .then(data => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar)
+    })
+    .catch(e=>console.log(e));
+  });
+
   return (
     <main className="main">
       <section className="profile">
         <button className="profile__avatar" type="button" onClick={onEditAvatarClick}>
-          <img className="profile__image" src={avatar} alt="profile" />
+          <img className="profile__image" src={userAvatar ? userAvatar : avatar } alt="profile" />
         </button>
         <div className="profile__info">
-          <h1 className="profile__name">Please be patient</h1>
+          <h1 className="profile__name">{userName ? userName : 'Please be patient'}</h1>
           <button className="profile__edit-button" type="button" aria-label="Edit profile" onClick={onEditProfileClick} />
-          <p className="profile__bio">We are fetching your user info...</p>
+          <p className="profile__bio">{userDecription ? userDecription : 'We are fetching your user info...'}</p>
         </div>
         <button className="profile__add-button"  type="button" aria-label="Add an item" onClick={onAddPlaceClick} />
       </section>
